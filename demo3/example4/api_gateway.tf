816e7ffa214242ee0a12d575b6ea6fb70535d46d
Luca Cito <ilyd89@yahoo.it>
2020-10-03 16:18:38 +0200

    Demo 3, example 4

---
+++ demo3/example4/api_gateway.tf
@@ -0,0 +1,53 @@
+resource "aws_api_gateway_rest_api" "my_gateway" {
+  name        = "ServerlessExample"
+  description = "TecObserver Terraform Example"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.my_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.my_gateway.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_example.invoke_arn
+}
+
+# Root methods & integrations
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.my_gateway.id
+  resource_id   = aws_api_gateway_rest_api.my_gateway.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.my_gateway.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_example.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "my_gateway_deployment" {
+   depends_on = [
+     aws_api_gateway_integration.lambda,
+     aws_api_gateway_integration.lambda_root,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.my_gateway.id
+   stage_name  = "test"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.my_gateway_deployment.invoke_url
+}
---
+++ demo3/example4/lambda.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_lambda_function" "lambda_example" {
+  function_name = "ServerlessExample"
+
+  s3_bucket = "tecobserver-terraform-demo-ex3"
+  s3_key    = "v1/minimal-node.zip"
+
+  handler = "main.handler"
+  runtime = "nodejs10.x"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_gateway.id
+  parent_id   = aws_api_gateway_rest_api.my_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_lambda_permission" "permission_for_apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.lambda_example.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   source_arn = "${aws_api_gateway_rest_api.my_gateway.execution_arn}/*/*"
+}
+
